bef7bae75fc1124fd0db8a295db3e7a8b0bd4aac
Liudmyla Melnychuk <liudmyla_melnychuk@epam.com>
2026-05-03 02:55:38 +0200

    feat: update frontend API integration with product service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://lgo9l15k2c.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
