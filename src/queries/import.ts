df576b3975ce0bc5322d758d2c56d2ec3ed480a7
Liudmyla Melnychuk <liudmyla_melnychuk@epam.com>
2026-05-28 01:45:59 +0200

    feat: add Authorization header for import requests

--- src/queries/import.ts
+++ src/queries/import.ts
@@ -3,8 +3,13 @@ import API_PATHS from "~/constants/apiPaths";
 
 export const useImportFile = () => {
   const getSignedUrl = async (fileName: string) => {
+    const token = localStorage.getItem("authorization_token");
+
     const response = await axios.get<string>(`${API_PATHS.import}/import`, {
       params: { name: fileName },
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
     });
 
     return response.data;
